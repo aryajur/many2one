88b17532d8ae86ae0657a0db43954d68a907c1fc
aryajur <milind.gupta@gmail.com>
2021-09-20 15:28:26 -0700

    Added code to remove comments from a file before searching for require.

--- src/many2one.lua
+++ src/many2one.lua
@@ -42,8 +42,9 @@ deployDir = "../deploy/"
 ]]
 
 -- TODO:
--- 1. Remove commented text before doing require search
--- 2. Add option to have __MANY2ONEFILES global
+-- 1. Add option to have __MANY2ONEFILES global
+-- 2. Add file obfuscation
+-- 3. Add file compression
 
 require("submodsearcher")
 
@@ -77,7 +78,7 @@ local luaCode = {}
 local args = parser:parse()
 --local configFile = args[1] or "Config.lua"
 logger:info("------------------------------------------------------------------")
-logger:info("Many2One version 1.21.09.20")
+logger:info("Many2One version 1.21.09.20a")
 logger:info(" ")
 
 local txtExt = {"lua"}			-- List of file extensions that are text files and will be combined with the lua script file
@@ -138,6 +139,132 @@ tu.mergeArrays(moreExclude,exclude)
 if not mainFile then 
 	logger:error("Need a main file name to package. The configuration file should specify a mainFile.")
 	os.exit()
+end
+
+-- Function to remove comments from a lua file passed as a string
+local function removeComments(str)
+	-- Do a simple tokenizer for strings and comments
+	local strout = {}
+	local CODE,STRING,COMMENT = 0,1,2
+	local strenc = ""
+	local comenc = ""
+	local token = CODE
+	local currChar 
+	local pos = 1
+	local lastToken = 0
+	while pos <= #str do
+		currChar = str:sub(pos,pos)
+		if currChar == "-" and token ~= STRING and token ~= COMMENT then
+			-- Possibility of starting the comment
+			if str:sub(pos + 1,pos + 1) == "-" then
+				-- This is definitely starting of a comment
+				token = COMMENT
+				-- Add all code till now before starting the comment
+				strout[#strout + 1] = str:sub(lastToken + 1,pos-1)
+				pos = pos + 1
+				lastToken = pos
+				-- Check if this is a square bracket comment
+				local strt,stp = str:find("%[=*%[",pos+1)
+				if strt and strt == pos + 1 then
+					-- This is a square bracket comment
+					comenc = str:sub(pos+1,stp):gsub("%[","]")
+					print("Square bracket comment at "..strt.." ending with "..comenc)
+					pos = stp
+					lastToken = pos
+				else
+					comenc = "--"
+				end
+			end
+		elseif currChar == "]" and (token == COMMENT or token == STRING) then
+			-- Possibility of ending a comment or a string
+			local enc
+			if token == COMMENT then 
+				enc = comenc
+			else
+				enc = strenc
+			end
+			local strt,stp = str:find(enc,pos,true)
+			if strt and strt == pos then
+				-- Comment/string ends here
+				if token == STRING then
+					-- Add all the code till now till ending of string
+					strout[#strout + 1] = str:sub(lastToken + 1,pos-1+#strenc)
+					strenc = ""
+				else
+					comenc = ""
+				end
+				token = CODE
+				pos = pos - 1 + #enc
+				lastToken = pos
+			end
+		elseif currChar == "\n" and token == COMMENT then
+			-- Possibily of ending a comment
+			if comenc == "--" then
+				-- End the comment here
+				token = CODE
+				lastToken = pos-1	-- No code to add
+				comenc = ""
+			end
+		elseif currChar == "[" and token ~= COMMENT and token ~= STRING then
+			-- Possibility of starting a string
+			-- Check if this is a square bracket string
+			local strt,stp = str:find("%[=*%[",pos)
+			if strt and strt == pos then
+				-- This is a square bracket string
+				token = STRING
+				strenc = str:sub(pos,stp):gsub("%[","]")
+				pos = stp
+				-- Add all the code till now
+				strout[#strout + 1] = str:sub(lastToken + 1,pos)
+				lastToken = pos
+			end
+		elseif currChar == "'" and token ~= COMMENT then
+			-- Possibility of starting or ending a string
+			if token == STRING then
+				if strenc == "'" then
+					-- String ends here
+					token = CODE
+					strenc = ""
+					-- Add all the code till now
+					strout[#strout + 1] = str:sub(lastToken + 1,pos)
+					lastToken = pos
+				end
+			else
+				-- String starts here
+				token = STRING
+				strenc = "'"
+				-- Add all the code till now
+				strout[#strout + 1] = str:sub(lastToken + 1,pos)
+				lastToken = pos
+			end
+		elseif currChar == '"' and token ~= COMMENT then
+			-- Possibility of starting or ending a string
+			if token == STRING then
+				if strenc == '"' then
+					-- String ends here
+					token = CODE
+					strenc = ""
+					-- Add all the code till now
+					strout[#strout + 1] = str:sub(lastToken + 1,pos)
+					lastToken = pos
+				end
+			else
+				-- String starts here
+				token = STRING
+				strenc = '"'
+				-- Add all the code till now
+				strout[#strout + 1] = str:sub(lastToken + 1,pos)
+				lastToken = pos
+			end
+		elseif currChar == [[\]] and token == STRING then
+			-- Possibility of escaping the next character
+			if strenc == "'" or strenc == '"' then
+				pos = pos + 1	-- Escape the next character
+			end
+		end
+		pos = pos + 1
+	end
+	return table.concat(strout)
 end
 
 local fileQ = {
@@ -218,7 +345,7 @@ while #fileQ > 0 do
 	f:close()
 	reference = fileQ[1][2]
 	logger:info("File reading done.")
-	addAllRequires(fDat)
+	addAllRequires(removeComments(fDat))
 	-- Remove the item from fileQ
 	table.remove(fileQ,1)
 end
